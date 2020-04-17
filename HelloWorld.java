ed61365ef640f69f09cb0b7da6bb5c9a5310d733
Madhuri <dandeboinamadhuri@gmail.com>
2020-04-17 19:04:46 +0000

    webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello sainath!");
-    System.out.println("Hello, Madhu!");    
-    System.out.println("POLL SCM Modified!");    
-    System.out.println("POLL SCM !");    
+    System.out.println("WEBHOOK");
+   
+    
+   
   }
 }
