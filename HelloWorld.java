5e03fcb8999db5423b97903752eb49f472e2648f
Madhuri <dandeboinamadhuri@gmail.com>
2020-04-17 18:12:15 +0000

    Poll Scm modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello sainath!");
     System.out.println("Hello, Madhu!");    
-    System.out.println("POLL SCM!");    
+    System.out.println("POLL SCM Modified!");    
   }
 }
