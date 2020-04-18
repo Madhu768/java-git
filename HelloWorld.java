44bf66fd0020a89a24130e3132f0f1e6e06e0d35
Madhu768 <63680155+Madhu768@users.noreply.github.com>
2020-04-17 21:01:25 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("WEBHOOK Build Trigger");
-   
-    
+     System.out.println("Ngrok - jenkins -github");
    
   }
 }
